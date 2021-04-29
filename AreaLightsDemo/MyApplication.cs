217e75050fef8e5fa5b2c7fed7ecafb826cfcdcc
David Ávila <david.avila.membrives@gmail.com>
2021-04-29 17:09:31 +0200

    Updated to 3.2 preview

--- AreaLightsDemo/MyApplication.cs
+++ AreaLightsDemo/MyApplication.cs
@@ -5,7 +5,7 @@ using WaveEngine.Platform;
 
 namespace AreaLightsDemo
 {
-    public class MyApplication : Application
+    public partial class MyApplication : Application
     {
         public MyApplication()
         {
